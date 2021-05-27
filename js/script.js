e685d6b2fadd2e4acd7e66f74a20addb8109377a
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-05-27 15:01:21 +0200

    Closing project

--- js/script.js
+++ js/script.js
@@ -1,28 +1,33 @@
 // Lettura dei chilometri percorsi dall'utente
 var kmPercorsi = parseInt(prompt('Quanti chilometri percorrerai?'));
-console.log(kmPercorsi);
+// console.log(kmPercorsi);
+document.getElementById('km-percorsi').innerHTML += kmPercorsi + 'km';
 
 // Lettura dell'età del passeggero
 var eta = parseInt(prompt('Quanti anni hai?'));
-console.log(eta);
+// console.log(eta);
 
-// Definizione del prezzo al kilometro
+// Definizione del prezzo al kilometro e degli sconti
 var prezzoAlKm = 0.21;
-// sconto del 20% per i minori
+document.getElementById('prezzo-al-km').innerHTML += prezzoAlKm + '€';
+
 var scontoMinorenne = 0.8;
-// sconto per gli over 65
 var scontoAnziano = 0.6;
 
+
 // Calcolo del prezzo totale
-var totale = kmPercorsi * prezzoAlKm;
+var totale = (kmPercorsi * prezzoAlKm).toFixed(2);
+document.getElementById('prezzo-totale').innerHTML += totale + '€';
 
 // check se necessaria l'applicazione di uno sconto
 if (eta < 18) {
     // se minorenne
-    totale = totale * scontoMinorenne;
-} else if (eta >= 65) {
+    totale = (totale * scontoMinorenne).toFixed(2);
+    document.getElementById('prezzo-scontato').innerHTML = 'Essendo minorenne hai diritto ad uno sconto del 20%. Il tuo prezzo scontato è ==> ' + totale + '€';
+} else if (eta > 65) {
     // se over 65
-    totale = totale * scontoAnziano;
+    totale = (totale * scontoAnziano).toFixed(2);
+    document.getElementById('prezzo-scontato').innerHTML = 'Essendo over 65 hai diritto ad uno sconto del 40%. Il tuo prezzo scontato è ==> ' + totale + '€';
 }
 
-console.log(totale + '€');
+// console.log(totale + '€');
